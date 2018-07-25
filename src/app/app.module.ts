e22e6b5fd8e57837ba6b1a2441371473d27756bb
Derick Beckwith <derick.beckwith@gmail.com>
2018-07-24 23:27:15 -0400

    Import calendar and scheduler modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CalendarModule } from 'angular-calendar';
+import { SchedulerModule } from 'angular-calendar-scheduler';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule.forRoot(),
+    SchedulerModule.forRoot({ locale: 'en', headerDateFormat: 'daysRange' }),
   ],
   providers: [],
   bootstrap: [AppComponent]
